ed3282cfd43031a42e0ae2765c379a5d2f54fa3c
GabrielRyan1999 <gabrielryan1999@gmail.com>
2020-07-23 20:02:33 +0700

    v1

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,65 @@ Route::get('/', function(){
 Route::get('/agama', function(){
     return view('templates.Matkul.agama');
 });
-Route::get('/ind', function(){
-    return view('templates.Matkul.ind');
+Route::get('/antropologi', function(){
+    return view('templates.Matkul.antropologi');
+});
+Route::get('/indo', function(){
+    return view('templates.Matkul.indo');
+});
+Route::get('/inggris', function(){
+    return view('templates.Matkul.inggris');
+});
+Route::get('/bk', function(){
+    return view('templates.Matkul.bk');
+});
+Route::get('/biologi', function(){
+    return view('templates.Matkul.biologi');
+});
+Route::get('/ekonomi', function(){
+    return view('templates.Matkul.ekonomi');
+});
+Route::get('/fisika', function(){
+    return view('templates.Matkul.fisika');
+});
+Route::get('/geografi', function(){
+    return view('templates.Matkul.geografi');
+});
+Route::get('/jerman', function(){
+    return view('templates.Matkul.jerman');
+});
+Route::get('/kimia', function(){
+    return view('templates.Matkul.kimia');
+});
+Route::get('/mandarin', function(){
+    return view('templates.Matkul.mandarin');
+});
+Route::get('/matematika', function(){
+    return view('templates.Matkul.matematika');
+});
+Route::get('/penjas', function(){
+    return view('templates.Matkul.penjas');
+});
+Route::get('/pendidikannilai', function(){
+    return view('templates.Matkul.pendidikannilai');
+});
+Route::get('/perancis', function(){
+    return view('templates.Matkul.perancis');
+});
+Route::get('/pkn', function(){
+    return view('templates.Matkul.pkn');
+});
+Route::get('/pkwu', function(){
+    return view('templates.Matkul.pkwu');
+});
+Route::get('/sejarah', function(){
+    return view('templates.Matkul.sejarah');
+});
+Route::get('/senirupa', function(){
+    return view('templates.Matkul.senirupa');
+});
+Route::get('/sosiologi', function(){
+    return view('templates.Matkul.sosiologi');
 });
 Route::get('/profile', function(){
     return view('templates.profile');
